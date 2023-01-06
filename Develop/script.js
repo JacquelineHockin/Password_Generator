a4110b8a267d89e4c852a2bb3f6fedd9cadcd0d9
JacquelineHockin <jacquelinehcashman@gmail.com>
2023-01-05 14:55:50 -1000

    added to script.js

--- Develop/script.js
+++ Develop/script.js
@@ -1,19 +1,31 @@
 // Assignment code here
 var generateBtn = document.querySelector("#generate");
 
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+// start of function for password generation //
+function generatePassword() {
+  // boolean variables for prompts //
+  var passLength = prompt(
+    "How long will your password be? You must pick a number between 8 and 128."
+  );
+  var upperCase = confirm("Will your password use uppercase?");
+  var lowerCase = confirm("Will your password use lowercase?");
+  var numbers = confirm("Will your password use numbers?");
+  var specialCh = confirm("Will your password use special characters?");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  // Get references to the #generate element
+  var generateBtn = document.querySelector("#generate");
 
-  passwordText.value = password;
+  // Write password to the #password input
+  function writePassword() {
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
 
-}
+    passwordText.value = password;
+  }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+  // Add event listener to generate button
+  generateBtn.addEventListener("click", writePassword);
 
+  ////////////////////////////////////////////////////////////////////////////
 
+  
